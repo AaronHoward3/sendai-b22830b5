9fac3be4f37475e59680c3f377f7bc3cb48e7757
Aaron Howard <aaronjhoward3@gmail.com>
2025-08-25 23:58:33 -0500

    Ui fixes with font capitalizations

--- apps/web/src/components/Navigation.tsx
+++ apps/web/src/components/Navigation.tsx
@@ -67,7 +67,7 @@ const Navigation: React.FC<NavigationProps> = ({ onHomeClick }) => {
         </Button>
 
         {/* Center Title */}
-        <div className="text-lg font-zen text-foreground">Irios A.I.</div>
+        <div className="text-lg font-zen text-foreground">IRIOS A.I.</div>
 
         {/* Profile Dropdown (click to open) */}
         <DropdownMenu>
